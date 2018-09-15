935c3229345ada3c3b3228744f2ad5944b45a8f1
Vishv Patel <Vishvpatel@v1020-wn-233-150.campus-dynamic.uwaterloo.ca>
2018-09-15 19:53:40 -0400

    Addition to speechrecog file

--- SpeechRecog.py
+++ SpeechRecog.py
@@ -4,6 +4,7 @@ tempfile = sr.AudioFile('OSR_us_000_0010_8k.wav')
 r = sr.Recognizer()
 
 with tempfile as tf:
+	r.adjust_for_ambient_noise(tf, duration=0.5)
 	audio = r.record(tf)
 	out = r.recognize_google(audio)
 	print(out)
