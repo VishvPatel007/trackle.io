4b04e56335fab2f05936a0ac89014495bfad33a7
Ryan Min <ryanmin42@gmail.com>
2018-09-15 02:24:24 -0400

    add webcam functions

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
 import pyautogui
 import cv2
-import matplotlib
-import numpy
+import numpy as np
+from matplotlib import pyplot as plt
+
+cap = cv2.VideoCapture(0)
+ 
+while(True):
+    ret, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+ 
+    cv2.imshow('frame',gray)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
