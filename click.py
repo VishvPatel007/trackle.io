4bce05ee70b5ef8d8f6b88666811d5a13d102709
Vishv Patel <Vishvpatel@v1020-wn-233-150.campus-dynamic.uwaterloo.ca>
2018-09-16 04:08:27 -0400

    click speech recognition for use with the main.py file

---
+++ click.py
@@ -0,0 +1,43 @@
+import speech_recognition as sr
+import pyautogui as pya
+
+
+def recognize_speech_from_mic(recognizer, microphone):
+    # check that recognizer and microphone arguments are appropriate type
+    if not isinstance(recognizer, sr.Recognizer):
+        raise TypeError("`recognizer` must be `Recognizer` instance")
+
+    if not isinstance(microphone, sr.Microphone):
+        raise TypeError("`microphone` must be `Microphone` instance")
+
+    with microphone as source:
+        recognizer.adjust_for_ambient_noise(source)
+        audio = recognizer.listen(source)
+
+    # set up the response object
+    response = {
+        "success": True,
+        "error": None,
+        "transcription": None
+    }
+
+    try:
+        response["transcription"] = recognizer.recognize_google(audio)
+    except sr.RequestError:
+        # API was unreachable or unresponsive
+        response["success"] = False
+        response["error"] = "API unavailable"
+    except sr.UnknownValueError:
+        # speech was unintelligible
+        response["error"] = "Unable to recognize speech"
+
+    return response
+r = sr.Recognizer()
+m = sr.Microphone()
+words = recognize_speech_from_mic(r, m)
+
+while words["transcription"] != "stop":
+    words = recognize_speech_from_mic(r, m)
+    print("User said: {}".format(words["transcription"]))
+    pya.typewrite(words["transcription"])
+   
